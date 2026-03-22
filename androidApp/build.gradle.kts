322700ca62295c33bc3ccccfcc3766368fabe937
Paul Linca <paultudorlinca@gmail.com>
2026-03-22 17:10:59 +0100

    CS | Release

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.linca.courtscorewear"
         minSdk = 30
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.3.0"
+        versionCode = 11
+        versionName = "1.3.1"
     }
 
     androidResources {
